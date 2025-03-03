98ddacecdef822fdc42776c57a81cdb7949220ed
XxDrancerxX <idmdiazmora98@gmail.com>
2025-03-03 21:06:56 +0000

    spcaes created

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,27 +2,26 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/index.css";
 
+
+const sec = [1,2,3,4,5,6,7];
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row d-flex justify-content-evenly ms-5 me-5 ">
+		 {sec.map(
+				(seconds) => (
+					<div className="space col-auto mt-4 mb-4">{seconds}</div>
+					
+				)
+			)}  
+             </div>
 		</div>
 	);
 };
 
 export default Home;
+
+// {/* <i className="fa-solid fa-clock fa-6x"></i> */}
